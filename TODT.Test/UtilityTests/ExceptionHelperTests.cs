19be554e3ddd5f07a19e6138c276f4a6816c4a10
Tim <tduncan72@gmail.com>
2014-08-10 13:39:42 -0400

    Added Exception Helpers
    Added ExceptionHelpers
    Updated version to 3.0.2

---
+++ TODT.Test/UtilityTests/ExceptionHelperTests.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TOTD.Utility.ExceptionHelpers;
+
+namespace TODT.Test.UtilityTests
+{
+    [TestClass]
+    public class ExceptionHelperTests
+    {
+        [TestClass]
+        public class ThrowIfTests
+        {
+            [TestMethod]
+            public void IsNullDoesNotThrowExceptionForNonNullValue()
+            {
+                string argument = "test";
+                ThrowIf.Argument.IsNull(argument, "argument");
+            }
+
+            [TestMethod]
+            [ExpectedException(typeof(ArgumentNullException))]
+            public void IsNullThrowsExceptionForNullValue()
+            {
+                string argument = null;
+                ThrowIf.Argument.IsNull(argument, "argument");
+            }
+
+            [TestMethod]
+            public void IsLessThanDoesNotThrowExceptionIfArgumentExceedsValue()
+            {
+                int argument = 1;
+                ThrowIf.Argument.IsLessThan(argument, "argument", 0);
+            }
+
+            [TestMethod]
+            public void IsLessThanDoesNotThrowExceptionIfArgumentEqualsValue()
+            {
+                int argument = 0;
+                ThrowIf.Argument.IsLessThan(argument, "argument", 0);
+            }
+
+            [TestMethod]
+            [ExpectedException(typeof(ArgumentOutOfRangeException))]
+            public void IsLessThrowsExceptionIfArgumentIsLessThanValue()
+            {
+                int argument = -1;
+                ThrowIf.Argument.IsLessThan(argument, "argument", 0);
+            }
+        }
+    }
+}
---
+++ TOTD.Utility/ExceptionHelpers/ThrowIf.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+ 
+namespace TOTD.Utility.ExceptionHelpers
+{
+    public static class ThrowIf
+    {
+        public static class Argument
+        {
+            public static void IsNull(object argument, string argumentName)
+            {
+                if (argument == null)
+                {
+                    throw new ArgumentNullException(argumentName);
+                }
+            }
+
+            public static void IsLessThan(int argument, string argumentName, int value)
+            {
+                if (argument < value)
+                {
+                    throw new ArgumentOutOfRangeException(argumentName, value, null);
+                }
+            }
+        }
+    }
+}
