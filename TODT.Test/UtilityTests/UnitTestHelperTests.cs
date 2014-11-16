75188f34148ae15385bcd26a90f791ce82fe5123
Tim <tduncan72@gmail.com>
2014-11-16 12:32:21 -0500

    Added method for finding all async methods that return void

---
+++ TODT.Test/UtilityTests/UnitTestHelperTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TOTD.Utility.Misc;
+
+namespace TODT.Test.UtilityTests
+{
+    [TestClass]
+    public class UnitTestHelperTests
+    {
+        [TestMethod]
+        public void GetAsyncVoidMethodsReturnsAsyncVoidMethods()
+        {
+            IEnumerable<MethodInfo> methods = UnitTestHelper.GetAsyncVoidMethods(GetType().Assembly);
+            methods.Any(x => x.Name == "BadAsyncMethod").Should().BeTrue();
+            methods.Any(x => x.Name == "GoodAsyncMethod").Should().BeFalse();
+        }
+
+        private async void BadAsyncMethod()
+        {
+        }
+
+        private async Task GoodAsyncMethod()
+        {
+        }
+    }
+}
---
+++ TOTD.Utility/Misc/UnitTestHelper.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TOTD.Utility.Misc
+{
+    public static class UnitTestHelper
+    {
+        public static IEnumerable<MethodInfo> GetAsyncVoidMethods(Assembly assembly)
+        {
+            return
+                from t in assembly.GetTypes()
+                from m in t.GetMethods(BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Instance | BindingFlags.Static | BindingFlags.DeclaredOnly)
+                where Attribute.GetCustomAttribute(m, typeof(AsyncStateMachineAttribute)) != null && m.ReturnType == typeof(void)
+                select m;
+        }
+    }
+}
