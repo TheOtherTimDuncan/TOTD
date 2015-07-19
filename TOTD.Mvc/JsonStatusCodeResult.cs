683c05d5091ced14690bdd3b9988ae2d650c7632
Tim <tduncan72@gmail.com>
2015-07-19 18:09:53 -0400

    Add JsonStatusCodeResult

---
+++ TOTD.Mvc/JsonStatusCodeResult.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web.Mvc;
+
+namespace TOTD.Mvc
+{
+    internal static class JsonStatusCodeResultExtensions
+    {
+        public static JsonStatusCodeResult JsonStatusCode(this IController controller, HttpStatusCode statusCode, object data)
+        {
+            return controller.JsonStatusCode((int)statusCode, data);
+        }
+
+        public static JsonStatusCodeResult JsonStatusCode(this IController controller, int statusCode, object data)
+        {
+            return new JsonStatusCodeResult()
+            {
+                Data = data,
+                StatusCode = statusCode
+            };
+        }
+    }
+
+    public class JsonStatusCodeResult : JsonResult
+    {
+        public int? StatusCode
+        {
+            get;
+            set;
+        }
+
+        public override void ExecuteResult(ControllerContext context)
+        {
+            base.ExecuteResult(context);
+
+            if (StatusCode != null)
+            {
+                context.HttpContext.Response.StatusCode = StatusCode.Value;
+            }
+        }
+    }
+}
