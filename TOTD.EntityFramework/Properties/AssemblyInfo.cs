6ac009e5bfeb9925bad2f5a25c76a25f934c8a2f
Tim <tduncan72@gmail.com>
2015-07-19 18:09:06 -0400

    Add project for shared code for Entity Framework

---
+++ TOTD.EntityFramework/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("TOTD.EntityFramework")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyProduct("TOTD.EntityFramework")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("53533c2f-b859-4916-b727-6e9440fd602f")]
