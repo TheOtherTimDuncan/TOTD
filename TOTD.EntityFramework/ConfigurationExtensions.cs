400b75a5e39a3e9127f50ad7a87c0dc493b7ffb9
Tim <tduncan72@gmail.com>
2015-08-08 18:23:35 -0400

    Add extension methods for creating indexes in EF configurations

---
+++ TOTD.EntityFramework/ConfigurationExtensions.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity.Infrastructure.Annotations;
+using System.Data.Entity.ModelConfiguration.Configuration;
+using System.Linq;
+
+namespace TOTD.EntityFramework
+{
+    public static class ConfigurationExtensions
+    {
+        public static PrimitivePropertyConfiguration HasIndex(this PrimitivePropertyConfiguration configuration)
+        {
+            return configuration.HasColumnAnnotation("Index", new IndexAnnotation(new IndexAttribute()));
+        }
+
+        public static PrimitivePropertyConfiguration HasIndex(this PrimitivePropertyConfiguration configuration, string name)
+        {
+            return configuration.HasColumnAnnotation("Index", new IndexAnnotation(new IndexAttribute(name)));
+        }
+
+        public static PrimitivePropertyConfiguration HasUniqueIndex(this PrimitivePropertyConfiguration configuration)
+        {
+            return configuration.HasColumnAnnotation("Index", new IndexAnnotation(new IndexAttribute()
+            {
+                IsUnique = true
+            }));
+        }
+
+        public static PrimitivePropertyConfiguration HasUniqueIndex(this PrimitivePropertyConfiguration configuration, string name)
+        {
+            return configuration.HasColumnAnnotation("Index", new IndexAnnotation(new IndexAttribute(name)
+            {
+                IsUnique = true
+            }));
+        }
+    }
+}
