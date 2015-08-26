536778135a58f4766f0596323236b2088d4eafe6
Tim <tduncan72@gmail.com>
2015-08-26 18:52:57 -0400

    Add extension methods for TimeSpan

---
+++ TOTD.Utility/TimespanHelpers/TimespanExtensions.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TOTD.Utility.TimespanHelpers
+{
+    public static class TimespanExtensions
+    {
+        public static TimeSpan Days(this int days)
+        {
+            return new TimeSpan(days, 0, 0, 0);
+        }
+
+        public static TimeSpan Hours(this int hours)
+        {
+            return new TimeSpan(hours, 0, 0);
+        }
+
+        public static TimeSpan Minutes(this int minutes)
+        {
+            return new TimeSpan(0, minutes, 0);
+        }
+
+        public static TimeSpan Seconds(this int seconds)
+        {
+            return new TimeSpan(0, 0, seconds);
+        }
+
+        public static TimeSpan Milliseconds(this int milliseconds)
+        {
+            return new TimeSpan(0, 0, 0, 0, milliseconds);
+        }
+    }
+}
