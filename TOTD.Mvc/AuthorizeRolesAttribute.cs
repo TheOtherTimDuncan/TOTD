eb80aa8e450284a72fd59ea7060104b11f1f33a1
Tim <tduncan72@gmail.com>
2015-11-23 19:12:53 -0500

    Add attribute to make it easier to authorize multiple roles

---
+++ TOTD.Mvc/AuthorizeRolesAttribute.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web.Mvc;
+
+namespace TOTD.Mvc
+{
+    public class AuthorizeRolesAttribute : AuthorizeAttribute
+    {
+        public AuthorizeRolesAttribute()
+            : base()
+        {
+        }
+
+        public AuthorizeRolesAttribute(params string[] roles)
+        {
+            this.Roles = string.Join(",", roles);
+        }
+    }
+}
