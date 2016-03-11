48d2505d6690e1fb178f301f958029baa3527b26
Tim <tduncan72@gmail.com>
2016-03-10 19:06:42 -0500

    Add additional IQueryable extensions

---
+++ TODT.Test/QueryableExtensionTests.cs
@@ -0,0 +1,234 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EntityFramework.Testing.Moq;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TOTD.EntityFramework;
+
+namespace TODT.Test
+{
+    [TestClass]
+    public class QueryableExtensionTests
+    {
+        [TestClass]
+        public class TakePageMethod
+        {
+            [TestMethod]
+            public void ReturnsCorrectPage()
+            {
+                IQueryable<int> test = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 }.AsQueryable();
+                int[] result = test.TakePage(2, 5).ToArray();
+                result.Should().Equal(new[] { result[0], result[1], result[2], result[3], result[4] });
+            }
+
+            [TestMethod]
+            public void ThrowsExceptionForNullSource()
+            {
+                IQueryable<int> test = null;
+                Action action = () =>
+                {
+                    IQueryable<int> result = test.TakePage(1, 10);
+                };
+                action.ShouldThrow<ArgumentNullException>().And.ParamName.Should().Be("source");
+            }
+        }
+
+        [TestClass]
+        public class SelectToListMethod : QueryableExtensionTests
+        {
+            [TestMethod]
+            public void ReturnsExecutedProjection()
+            {
+                TestEntity testEntity1 = new TestEntity()
+                {
+                    ID = 1,
+                    Name = "name1"
+                };
+
+                TestEntity testEntity2 = new TestEntity()
+                {
+                    ID = 2,
+                    Name = "name2"
+                };
+
+                MockDbSet<TestEntity> mockDbSet = CreateMockDbSet(testEntity1, testEntity2);
+                var result = mockDbSet.Object.SelectToList(x => x.ID.Value);
+                result.Should().BeOfType<List<int>>();
+                result.Should().Equal(new[] { testEntity1.ID, testEntity2.ID });
+            }
+
+            [TestMethod]
+            public async Task ReturnsExecutedProjectionAsynchronously()
+            {
+                TestEntity testEntity1 = new TestEntity()
+                {
+                    ID = 1,
+                    Name = "name1"
+                };
+
+                TestEntity testEntity2 = new TestEntity()
+                {
+                    ID = 2,
+                    Name = "name2"
+                };
+
+                MockDbSet<TestEntity> mockDbSet = CreateMockDbSet(testEntity1, testEntity2);
+                var result = await mockDbSet.Object.SelectToListAsync(x => x.ID.Value);
+                result.Should().BeOfType<List<int>>();
+                result.Should().Equal(new[] { testEntity1.ID, testEntity2.ID });
+            }
+        }
+
+        [TestClass]
+        public class SelectSingleMethod : QueryableExtensionTests
+        {
+            [TestMethod]
+            public void ReturnsExecutedProjection()
+            {
+                TestEntity testEntity1 = new TestEntity()
+                {
+                    ID = 1,
+                    Name = "name1"
+                };
+
+                TestEntity testEntity2 = new TestEntity()
+                {
+                    ID = 2,
+                    Name = "name2"
+                };
+
+                MockDbSet<TestEntity> mockDbSet = CreateMockDbSet(testEntity1, testEntity2);
+                var result = mockDbSet.Object.Where(x => x.ID == 1).SelectSingle(x => x.ID);
+                result.Should().Be(1);
+            }
+
+            [TestMethod]
+            public async Task ReturnsExecutedProjectionAsynchronously()
+            {
+                TestEntity testEntity1 = new TestEntity()
+                {
+                    ID = 1,
+                    Name = "name1"
+                };
+
+                TestEntity testEntity2 = new TestEntity()
+                {
+                    ID = 2,
+                    Name = "name2"
+                };
+
+                MockDbSet<TestEntity> mockDbSet = CreateMockDbSet(testEntity1, testEntity2);
+                var result = await mockDbSet.Object.Where(x => x.ID == 1).SelectSingleAsync(x => x.ID);
+                result.Should().Be(1);
+            }
+        }
+
+        [TestClass]
+        public class SelectSingleOrDefaultMethod : QueryableExtensionTests
+        {
+            [TestMethod]
+            public void ReturnsExecutedProjection()
+            {
+                TestEntity testEntity1 = new TestEntity()
+                {
+                    ID = 1,
+                    Name = "name1"
+                };
+
+                TestEntity testEntity2 = new TestEntity()
+                {
+                    ID = 2,
+                    Name = "name2"
+                };
+
+                MockDbSet<TestEntity> mockDbSet = CreateMockDbSet(testEntity1, testEntity2);
+                var result = mockDbSet.Object.Where(x => x.ID == 1).SelectSingleOrDefault(x => x.ID);
+                result.Should().Be(1);
+            }
+
+            [TestMethod]
+            public async Task ReturnsExecutedProjectionAsynchronously()
+            {
+                TestEntity testEntity1 = new TestEntity()
+                {
+                    ID = 1,
+                    Name = "name1"
+                };
+
+                TestEntity testEntity2 = new TestEntity()
+                {
+                    ID = 2,
+                    Name = "name2"
+                };
+
+                MockDbSet<TestEntity> mockDbSet = CreateMockDbSet(testEntity1, testEntity2);
+                var result = await mockDbSet.Object.Where(x => x.ID == 1).SelectSingleOrDefaultAsync(x => x.ID);
+                result.Should().Be(1);
+            }
+
+            [TestMethod]
+            public void ReturnsDefaultValueIfProjectionIsEmpty()
+            {
+                TestEntity testEntity1 = new TestEntity()
+                {
+                    ID = 1,
+                    Name = "name1"
+                };
+
+                TestEntity testEntity2 = new TestEntity()
+                {
+                    ID = 2,
+                    Name = "name2"
+                };
+
+                MockDbSet<TestEntity> mockDbSet = CreateMockDbSet(testEntity1, testEntity2);
+                var result = mockDbSet.Object.Where(x => x.ID == 1).SelectSingleOrDefault(x => x.ID);
+                result.Should().Be(1);
+            }
+
+            [TestMethod]
+            public async Task ReturnsDefaultValueAsynchronouslyIfProjectionIsEmpty()
+            {
+                TestEntity testEntity1 = new TestEntity()
+                {
+                    ID = 1,
+                    Name = "name1"
+                };
+
+                TestEntity testEntity2 = new TestEntity()
+                {
+                    ID = 2,
+                    Name = "name2"
+                };
+
+                MockDbSet<TestEntity> mockDbSet = CreateMockDbSet(testEntity1, testEntity2);
+                var result = await mockDbSet.Object.Where(x => x.ID == 1).SelectSingleOrDefaultAsync(x => x.ID);
+                result.Should().Be(1);
+            }
+        }
+
+        private MockDbSet<TestEntity> CreateMockDbSet(params TestEntity[] entities)
+        {
+            return new MockDbSet<TestEntity>()
+                .SetupLinq()
+                .SetupSeedData(entities);
+        }
+
+        public class TestEntity
+        {
+            public int? ID
+            {
+                get;
+                set;
+            }
+
+            public string Name
+            {
+                get;
+                set;
+            }
+        }
+    }
+}
--- TOTD.EntityFramework/QueryableExtensions.cs
+++ TOTD.EntityFramework/QueryableExtensions.cs
@@ -10,6 +10,14 @@ namespace TOTD.EntityFramework
 {
     public static class QueryableExtensions
     {
+        /// <summary>
+        /// Returns the specified number of elements starting at the beginning of the specified page
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="source"></param>
+        /// <param name="page">The 1-based number of the page to skip to</param>
+        /// <param name="pageSize">The number of elements in a page</param>
+        /// <returns></returns>
         public static IQueryable<T> TakePage<T>(this IQueryable<T> source, int? page, int pageSize)
         {
             ThrowIf.Argument.IsNull(source, "source");
@@ -20,6 +28,14 @@ namespace TOTD.EntityFramework
             return source.Skip(() => skipCount).Take(() => pageSize);
         }
 
+        /// <summary>
+        /// Returns the specified number of elements starting at the beginning of the specified page and immediately invokes the query by calling ToListAsync
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="source"></param>
+        /// <param name="page">The 1-based number of the page to skip to</param>
+        /// <param name="pageSize">The number of elements in a page</param>
+        /// <returns></returns>
         public static async Task<IEnumerable<T>> TakePageToListAsync<T>(this IQueryable<T> source, int? page, int pageSize)
         {
             ThrowIf.Argument.IsNull(source, "source");
@@ -57,6 +73,64 @@ namespace TOTD.EntityFramework
         public static async Task<IEnumerable<TResult>> SelectToListAsync<TSource, TResult>(this IQueryable<TSource> source, Expression<Func<TSource, TResult>> selector)
         {
             return await source.Select(selector).ToListAsync();
+        }
+
+        /// <summary>
+        /// Projects each element of a sequence into a new form and returns the only element from the new form
+        /// Throws an exception if there is not exactly one element in the new form
+        /// </summary>
+        /// <typeparam name="TSource"></typeparam>
+        /// <typeparam name="TResult"></typeparam>
+        /// <param name="source"></param>
+        /// <param name="selector"></param>
+        /// <returns>The single element of the new form</returns>
+        public static TResult SelectSingle<TSource, TResult>(this IQueryable<TSource> source, Expression<Func<TSource, TResult>> selector)
+        {
+            return source.Select(selector).Single();
+        }
+
+        /// <summary>
+        /// Projects each element of a sequence into a new form and returns the only element from the new form
+        /// Throws an exception if there is not exactly one element in the new form
+        /// </summary>
+        /// <typeparam name="TSource"></typeparam>
+        /// <typeparam name="TResult"></typeparam>
+        /// <param name="source"></param>
+        /// <param name="selector"></param>
+        /// <returns>The single element of the new form</returns>
+        public static async Task<TResult> SelectSingleAsync<TSource, TResult>(this IQueryable<TSource> source, Expression<Func<TSource, TResult>> selector)
+        {
+            return await source.Select(selector).SingleAsync();
+        }
+
+        /// <summary>
+        /// Projects each element of a sequence into a new form and returns the only element from the new form
+        /// or a default value if the new form is empty
+        /// Throws an exception if there is not exactly one element in the new form
+        /// </summary>
+        /// <typeparam name="TSource"></typeparam>
+        /// <typeparam name="TResult"></typeparam>
+        /// <param name="source"></param>
+        /// <param name="selector"></param>
+        /// <returns>The single element of the new form or default(TSource) if the new form is empty</returns>
+        public static TResult SelectSingleOrDefault<TSource, TResult>(this IQueryable<TSource> source, Expression<Func<TSource, TResult>> selector)
+        {
+            return source.Select(selector).SingleOrDefault();
+        }
+
+        /// <summary>
+        /// Projects each element of a sequence into a new form and returns the only element from the new form
+        /// or a default value if the new form is empty
+        /// Throws an exception if there is not exactly one element in the new form
+        /// </summary>
+        /// <typeparam name="TSource"></typeparam>
+        /// <typeparam name="TResult"></typeparam>
+        /// <param name="source"></param>
+        /// <param name="selector"></param>
+        /// <returns>The single element of the new form or default(TSource) if the new form is empty</returns>
+        public static async Task<TResult> SelectSingleOrDefaultAsync<TSource, TResult>(this IQueryable<TSource> source, Expression<Func<TSource, TResult>> selector)
+        {
+            return await source.Select(selector).SingleOrDefaultAsync();
         }
     }
 }
